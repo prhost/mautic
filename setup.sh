84d1d60e30a3db79dc734f6f9b23dde1fd2a4fc9
Kallef Alexandre <kallef.alexandre@orbital.codes>
2025-08-15 13:42:36 -0300

    Add setup and update scripts for Mautic Docker

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script de setup do Mautic Docker
+# Execute este script da pasta raiz do projeto
+
+cd docker && ./scripts/setup.sh
---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script de update do Mautic Docker
+# Execute este script da pasta raiz do projeto após git pull
+
+cd docker && ./scripts/update.sh
